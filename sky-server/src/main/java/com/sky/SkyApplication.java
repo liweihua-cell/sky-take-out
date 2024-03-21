cebc64c4f1c54a32ccc8bbc6e4f9a79d7f509490
liweihua-cell <2410576785@qq.com>
2024-03-21 15:47:34 +0800

    springcloud feign的补充说明及部分依赖导入调整

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -10,9 +10,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 //@EnableDiscoveryClient 配置在启动类上实现springcloud服务的注册
-//有关springcloud的具体代码案例参考AddressBookService类
+//有关springcloud的具体代码案例参考AddressBookServiceImpl类
+//有关feign的远程调用的具体代码参考AddressBookService接口
 //sentinel相关的限流配置参考FilterContextConfig类,异常处理配置参考AddressBookServiceImpl类，规则持久化配置参考FilePersistence类
 //gateway部分请看api-gateway模块
+@EnableFeignClients//开启feign
 @EnableScheduling//开启任务调度
 public class SkyApplication {
     public static void main(String[] args) {
